f4fde9ae24d8570d9b05a1230ad216a7038e2d5e
aksanuraag <saikarthikanurag.k2015@vit.ac.in>
2016-11-12 10:33:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -57,10 +57,28 @@
         </div>
   </form>
 	<?php
-		if(isset()){
-			
+		$un = $_GET["username"];
+		$pw = $_GET["password"];
+		$conn = pg_pconnect("dbname=aksanuraag");
+		if (!$conn) {
+		  echo "An error occurred.\n";
+		  exit;
 		}
-		
+
+		$result = pg_query($conn, "SELECT username, password FROM users WHERE username='$un' AND password='$pw'");
+		if (!$result) {
+		  echo "An error occurred.\n";
+		  exit;
+		}
+
+		$rows = pg_num_rows($result);
+		if($rows == 1){
+			echo "Welcome " . $un;
+		}
+		else{
+			header("Location: http://aksanuraag.imad.hasura-app.io/");
+		}	
+	
 	?>
 </body>
 </html>
